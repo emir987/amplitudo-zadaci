eb5e8fcdb04acca9a1789a3865e63cfc50b084b7
Emir Kosuta <emir.kosuta@gmail.com>
2020-05-17 23:40:25 +0200

    validate form tooltip

--- js/custom-js.js
+++ js/custom-js.js
@@ -73,3 +73,122 @@
       }
 
   });
+
+
+
+
+  // =========================== form validation ================================
+
+  const form = document.getElementById('new_user_form');
+  const name = document.getElementById('name');
+  const surname = document.getElementById('surname');
+  const gender = document.getElementById('gender');
+  //select
+  const date = document.getElementById('date');
+  const month = document.getElementById('month');
+  const year = document.getElementById('year');
+
+
+  const state = document.getElementById('state');
+  const city = document.getElementById('city');
+  const zip = document.getElementById('zip');
+  //   const state = document.getElementById('state');
+  //   const state = document.getElementById('state');
+  //   const state = document.getElementById('state');
+
+  function isEmpty(element) {
+      return (element.value.length >= 2 ? true : false);
+  }
+
+  function removeInvaild(element) {
+      element.classList.remove('invalid-input');
+      $(element).attr('data-original-title', '');
+      $(element).tooltip('hide');
+
+  }
+
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let isOk = true;
+
+      //name validate
+      if (!isEmpty(name)) {
+          showError(name, 'Unesite vaše ime');
+          return;
+      } else {
+          removeInvaild(name);
+      }
+
+      //surname validate
+      if (!isEmpty(surname)) {
+          showError(surname, 'Unesite vaše prezime')
+          return;
+      } else {
+          removeInvaild(surname);
+      }
+
+
+
+      //   if (gender.value == 0) {
+      //       showError(gender, "Odaberite pol");
+      //       return;
+      //   } else {
+      //       removeInvaild(gender);
+      //   }
+
+      const checkboxes = document.getElementsByClassName('select-main');
+
+      //   console.log(checkboxes);
+
+      for (let item of checkboxes) {
+          if (item.value == 0) {
+              console.log('ja' + item.id)
+              let message = '';
+              switch (item.id) {
+                  case 'gender':
+                      message = 'Odaberite Vaš pol!'
+                      break;
+                  case 'date':
+                      message = 'Odaberite datum!'
+                      break;
+                  case 'month':
+                      message = 'Odaberite mjesec!'
+                      break;
+                  case 'year':
+                      message = 'Odaberite godinu!'
+                      break;
+                  case 'state':
+                      message = 'Odaberite državu!'
+                      break;
+                  case 'driving-licence':
+                      message = 'Odaberite tip vozačke dozvole!'
+                      break;
+              }
+              showError(item.nextSibling, message);
+              return;
+          } else {
+              removeInvaild(item.nextSibling);
+          }
+      }
+
+  })
+
+  function validateSelectElements() {
+
+  }
+
+
+  function showError(element, message) {
+      element.classList.add('invalid-input');
+
+      $(element).attr('data-original-title', message)
+
+      $(element).tooltip('show');
+
+      $(element).on('shown.bs.tooltip', function () {
+          element.scrollIntoView({
+              behavior: "smooth",
+              block: "center",
+          });
+      })
+  }
